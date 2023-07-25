32a4cd12f4d8efb3edac9c3608ee8f7cae0cf18c
Giannoni-Ezequiel <egiannoni@frba.utn.edu.ar>
2023-07-25 13:10:07 -0300

    JavaScript
    Agregando las primeras funciones al encriptador. Falta desarrollar la función copiar

---
+++ docs/encriptador.js
@@ -0,0 +1,51 @@
+const textArea = document.querySelector(".textArea1");
+const mensaje = document.querySelector(".textArea2");
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"; 
+}
+
+function encriptar(stringEncriptado){
+    let matrizText = [["e", "enter"],["i", "imes"],
+                    ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptado = stringEncriptado.toLowerCase();
+    for(let i = 0; i < matrizText.length; i++){
+        if(stringEncriptado.includes(matrizText[i][0])){
+            stringEncriptado = stringEncriptado.replaceAll(matrizText[i][0], matrizText[i][1])
+        }
+    }
+    /* console.table(matrizText); */
+    return stringEncriptado;
+}
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+}
+
+function desencriptar(stringDesencriptado){
+    let matrizText = [["e", "enter"],["i", "imes"],
+                    ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptado = stringDesencriptado.toLowerCase();
+    for(let i = 0; i < matrizText.length; i++){
+        if(stringDesencriptado.includes(matrizText[i][1])){
+            stringDesencriptado = stringDesencriptado.replaceAll(matrizText[i][1], matrizText[i][0])
+        }
+    }
+    /* console.table(matrizText); */
+    return stringDesencriptado;
+}
+
+function copiar(mensaje){
+
+}
+
+const btnCopiar = document.querySelector(".btn-copiar");
+    btnCopiar.addEventListener("click", copiar = () => {
+        var contenido = document.querySelector(".textArea2").textContent;
+        navigator.clipboard.writeText(contenido);
+    })
